1cb7dc2957670842500082356d8d918b935694b0
Milos Pjanic <pjanic.milos@gmail.com>
2016-05-04 12:57:07 -0700

    Create sam2BigWig.sh

---
+++ sam2BigWig.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#create bam
+samtools view -Sb  $1 > $(basemname $1 .sam).bam
+
+#create bed from bam, requires bedtools bamToBed
+bamToBed -i $(basemname $1 .sam).bam -split > accepted_hits.bed
+
+#download bedItemOverlapCount
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bedItemOverlapCount
+chmod 775 bedItemOverlapCount
+
+#fetch hg19 chromosome sizes
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/fetchChromSizes
+chmod 775 fetchChromSizes
+./fetchChromSizes hg19 > hg19.chrom.sizes
+
+#create plus and minus strand bedgraph
+cat accepted_hits.bed | sort -k1,1 | ./bedItemOverlapCount hg19 -chromSize=hg19.chrom.sizes stdin | sort -k1,1 -k2,2n > accepted_hits.bedGraph
+
+#download bedGraphToBigWig
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bedGraphToBigWig
+chmod 755 bedGraphToBigWig
+
+#create plus and minus strand bigwig
+./bedGraphToBigWig accepted_hits.bedGraph hg19.chrom.sizes $(basemname $1 .sam).bw
+
+#removing intermediery files
+rm accepted_hits.bed
+rm accepted_hits.bedGraph
+rm bedItemOverlapCount
+rm bedGraphToBigWig
+rm fetchChromSizes
+rm hg19.chrom.sizes
